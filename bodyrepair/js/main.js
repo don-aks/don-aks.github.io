1e21dc00d275d05e3c88dab1790e5fc39ce8ef29
donaks <aksunov1@gmail.com>
2019-11-20 12:00:41 +0200

    Add popup plugin

--- bodyrepair/js/main.js
+++ bodyrepair/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
 	$(".js-input__telephone").mask("+7 (9nn) nnn-nn-nn");
 	$('.popup-link').magnificPopup({
-			type: 'inline'
-		});
+		type: 'inline',
+		mainClass: 'mfp-with-zoom',
+	  zoom: {
+	    enabled: true,
+	    duration: 300,
+	    easing: 'ease-in-out',
+	    opener: function(openerElement) {
+	      return openerElement.is('img') ? openerElement : openerElement.find('img');
+	    }
+		}
+	});
 });
