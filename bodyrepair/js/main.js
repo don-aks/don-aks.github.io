0eee6906a5010a5708d7880e6df754fc5199e548
donaks <aksunov1@gmail.com>
2019-11-20 12:27:40 +0200

    Update popup effect

--- bodyrepair/js/main.js
+++ bodyrepair/js/main.js
@@ -2,14 +2,12 @@ $(function(){
 	$(".js-input__telephone").mask("+7 (9nn) nnn-nn-nn");
 	$('.popup-link').magnificPopup({
 		type: 'inline',
+		midClick: true,
 		mainClass: 'mfp-with-zoom',
 	  zoom: {
 	    enabled: true,
 	    duration: 300,
-	    easing: 'ease-in-out',
-	    opener: function(openerElement) {
-	      return openerElement.is('img') ? openerElement : openerElement.find('img');
-	    }
+	    easing: 'ease-in-out'
 		}
 	});
 });
