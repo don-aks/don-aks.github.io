74dd5ab8950fc4a2839c88bc47af3f3d7ac94b95
don-aks <aksunov1@gmail.com>
2019-11-25 07:05:32 +0200

    Fixed bugs

--- bodyrepair/js/main.js
+++ bodyrepair/js/main.js
@@ -6,7 +6,7 @@ $(function(){
 		mainClass: 'mfp-zoom-in'
 	});
 	$(".js-button-scroll").on("click", function (event) {
-		var id  = $(this).attr('href'),
+		var id  = $(this).attr('data-anchor'),
 		    anchor = $(id).offset().top;
 		$('body,html').animate({scrollTop: anchor}, 1000);
 	});
