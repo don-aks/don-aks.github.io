21f004f3f07c6b93014f6809b6635509446ed21c
don-aks <aksunov1@gmail.com>
2019-12-02 20:41:57 +0200

    static layout

--- friendsfamily/js/main.js
+++ friendsfamily/js/main.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		autoScrolling: true,
 		scrollHorizontally: true,
-		sectionSelector: '.js-section'
+		sectionSelector: '.js-section',
+		anchors:['info1', 'info2', 'info3', 'scheme', 'box', 'map', 'contact-page']
 	});
-	$(document).on('click', '.js-move-down', function(){
+	$(".js-move-down").on("click", function (){
 	  fullpage_api.moveSectionDown();
 	});
+	$(".js-btn-contact").on("click", function (){
+	  fullpage_api.moveTo('contact-page', 0);
+	});
+	$(".js-input__telephone").mask("+7 (nnn) nnn-nn-nn");
 });
