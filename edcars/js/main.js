cd338d77e8de7f06c26ed74007866c0214db7da3
don-aks <aksunov1@gmail.com>
2019-12-26 08:40:52 +0200

    update edcars

--- edcars/js/main.js
+++ edcars/js/main.js
@@ -1,3 +1,22 @@
 $(function() {
 	$(".js-mask-phone").mask("+7 (nnn) nnn-nn-nn");
+	$(".js-question").on('click', function(event) {
+		var ans = $(this).siblings('.js-answer');
+		ans.show(300);
+		/*
+			При фокусе должен появлятся элемент в DOM'е, js-answer__close-btn, с аттрибутами aria-label="Закрыть" title="Закрыть"
+			При клике на этот элемент должно закрываться всплывающее окошко.
+			2 способа:
+				1. Сделать функцию.
+				2. Сделать элемент unfocused.
+			Второй предпочтительнее для такого рода проекта, но нужно знать как сделать и первым способом.
+			Пока что я js особо не понимаю, поэтому отложу сложные штуки на более подходящее время :).
+
+			Также нужно сделать эффект при появлении. Подойдет animate.css или CSS код из magnific popup.
+		*/
+	});
+	/*$(".js-question").focusout(function(event) {
+		var ans = $(this).siblings('.js-answer');
+		ans.hide(300);
+	});*/
 })
