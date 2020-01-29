1a359343a39ae7e2e6cdbc7a95bd472df9563bd3
donaks <aksunov1@gmail.com>
2020-01-29 15:30:29 +0200

    adaptive layout wetalk

--- wetalk/js/main.js
+++ wetalk/js/main.js
@@ -1,3 +1,11 @@
+// burger
+let burger = document.getElementById('burger-menu'),
+		menu = document.getElementById('menu');
+
+burger.addEventListener('click', function() {
+	slideToggle(menu);
+})
+
 // Плавная прокрутка до якоря
 const anchors = document.querySelectorAll('.js-scroll');
 
@@ -6,7 +14,8 @@ for (let anchor of anchors) {
 		e.preventDefault();
 		
 		let anchorActive = document.querySelector('.js-scroll.active');
-		removeClass(anchorActive, 'active');
+		try {anchorActive.className = anchorActive.className.replace(/\bactive\b/g, "")}
+		catch (e) {};
 		
 		anchor.className += " " + "active";
 
@@ -19,40 +28,108 @@ for (let anchor of anchors) {
 	})
 }
 
-/*// Обновление активных классов при прокрутке до блока
-const sections = document.querySelectorAll('body > section[id]');
+// Обновление активных классов при прокрутке до блока
+// сделано криво, поэтому закоментировано.
+
+/*const sections = document.querySelectorAll('body > section[id]');
 
 window.addEventListener('scroll', function() {
 	for (let section of sections) {
-		if ( isVisible(section) ) {
+		if (isVisible(section)) {
 			let link = document.querySelector('.menu__lnk[href="#'+section.id+'"]'),
 					linkActive = document.querySelector('.menu__lnk.active');
-			removeClass(linkActive, 'active');
+
+			try {linkActive.className = linkActive.className.replace(/\bactive\b/g, "")}
+			catch (e) {};
 			
 			link.className += " " + "active";
 			break;
 		}
 	}
-});*/
+});
+
+function isVisible(element) {
+	let elementRect = element.getBoundingClientRect(),
+			scrollTop = document.getElementsByTagName("body")[0].scrollTop;
+
+	return elementRect.top + elementRect.height > scrollTop;
+}*/
 
 // autosize plugin
 autosize(document.querySelectorAll('.textarea-autosize'));
 
-function removeClass(element, className) {
-	try {
-		element.className = element.className.replace(className, "");
-	}
-	catch(e) {}
+
+// jquery slideToggle
+function slideUp(element, duration = 500) {
+
+	return new Promise(function (resolve, reject) {
+
+			element.style.height = element.offsetHeight + 'px';
+			element.style.transitionProperty = `height, margin, padding`;
+			element.style.transitionDuration = duration + 'ms';
+			element.offsetHeight;
+			element.style.overflow = 'hidden';
+			element.style.height = 0;
+			element.style.paddingTop = 0;
+			element.style.paddingBottom = 0;
+			element.style.marginTop = 0;
+			element.style.marginBottom = 0;
+			window.setTimeout(function () {
+					element.style.display = 'none';
+					element.style.removeProperty('height');
+					element.style.removeProperty('padding-top');
+					element.style.removeProperty('padding-bottom');
+					element.style.removeProperty('margin-top');
+					element.style.removeProperty('margin-bottom');
+					element.style.removeProperty('overflow');
+					element.style.removeProperty('transition-duration');
+					element.style.removeProperty('transition-property');
+					resolve(false);
+			}, duration)
+	})
+}
+function slideDown(element, duration = 500) {
+
+	return new Promise(function (resolve, reject) {
+
+			element.style.removeProperty('display');
+			let display = window.getComputedStyle(element).display;
+
+			if (display === 'none') 
+					display = 'block';
+
+			element.style.display = display;
+			let height = element.offsetHeight;
+			element.style.overflow = 'hidden';
+			element.style.height = 0;
+			element.style.paddingTop = 0;
+			element.style.paddingBottom = 0;
+			element.style.marginTop = 0;
+			element.style.marginBottom = 0;
+			element.offsetHeight;
+			element.style.transitionProperty = `height, margin, padding`;
+			element.style.transitionDuration = duration + 'ms';
+			element.style.height = height + 'px';
+			element.style.removeProperty('padding-top');
+			element.style.removeProperty('padding-bottom');
+			element.style.removeProperty('margin-top');
+			element.style.removeProperty('margin-bottom');
+			window.setTimeout(function () {
+					element.style.removeProperty('height');
+					element.style.removeProperty('overflow');
+					element.style.removeProperty('transition-duration');
+					element.style.removeProperty('transition-property');
+			}, duration)
+	})
 }
 
-function isVisible(element) {
-	let elementBoundary = element.getBoundingClientRect(),
-			header = document.querySelector('header.header'),
-			headerHeight = header.getBoundingClientRect().height;
- 
-  let top = elementBoundary.top,
-  		bottom = elementBoundary.bottom,
-  		height = elementBoundary.height;
- 
-  return (top + height >= 0) && (height + (window.innerHeight - headerHeight)  >= top);
+function slideToggle(element, duration = 500) {
+	if (window.getComputedStyle(element).display === 'none') {
+
+			return this.slideDown(element, duration);
+
+	} else {
+
+			return this.slideUp(element, duration);
+	}
 }
